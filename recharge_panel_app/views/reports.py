a5bdac2a20d378e3b5b0654f2214a49538ee810d
verna <varna.nair@vivainfomedia.com>
2016-05-08 04:04:43 +0530

    date_time changed to  ::timestamp AT TIME ZONE 'Asia/Kolkata'

--- recharge_panel_app/views/reports.py
+++ recharge_panel_app/views/reports.py
@@ -1,16 +1,10 @@
 __author__ = 'verna'
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib.auth.decorators import login_required
-import hashlib
-from django.conf import settings
-from recharge_panel_app.models import get_data
+from recharge_panel_app.models import get_list_of_user,get_data
 import requests
 import datetime
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
 # from recharge_panel.config import logging
 import traceback,json
 
@@ -22,6 +16,9 @@ def get_report(request):
                                    'session_data': request.session, "success": "false", "error": "error",
                                    })
     # request.session['role'] = 'user'
+    list_of_users = get_list_of_user({"user_role":request.session['role']})
+    print list_of_users
+    context_data.update({'list_of_users':list_of_users})
     template = loader.get_template('commons/index.html')
     return HttpResponse(template.render(context_data))
 
@@ -34,6 +31,7 @@ def recharge_data(request):
         display_start = request.POST.get('iDisplayStart',False)
         display_end = request.POST.get('iDisplayLength',False)
         search = request.POST.get('sSearch', '.*')
+        selected_user = request.POST.get('selected_user_id',False)
         event_start_date = event_end_date = ''
         if from_date:
             event_start_date = str(from_date.split('/')[2])+'-'+str(from_date.split('/')[0])+"-"+str(from_date.split('/')[1])
@@ -47,7 +45,7 @@ def recharge_data(request):
         if request.is_ajax() or request.method == 'POST':
                     main_data = get_data({'user_id':request.session['user_id'],'user_role':request.session['role'],'query':'recharge_data','event_start_date':event_start_date,
                                                'event_end_date':event_end_date,'limit':display_end,
-                                               'offset':display_start,'search':search})
+                                               'offset':display_start,'search':search,"selected_user_id":selected_user})
                     if isinstance(main_data,dict):
                         display_content = main_data['final_data']
                         total_count = main_data['total_count']
